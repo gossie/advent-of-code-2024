c631558d1d99617e1f96720e1da4455d44d73765
André Schreck <github@andreschreck.de>
2024-12-04 11:23:34 +0100

    day 2

--- adventofcode.go
+++ adventofcode.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "time"
     "github.com/gossie/adventofcode2024/day1"
+    "github.com/gossie/adventofcode2024/day2"
 )
 
 func main() {
@@ -16,4 +17,12 @@ func main() {
     startday1Part2 := time.Now().UnixMilli()
     day1Part2 := day1.Part2("day1/day1.txt")
     fmt.Println("day1, task 2: ", day1Part2, ", took", (time.Now().UnixMilli() - startday1Part2), "ms")
+
+    fmt.Println("\nPerforming tasks of day2")
+    startday2Part1 := time.Now().UnixMilli()
+    day2Part1 := day2.Part1("day2/day2.txt")
+    fmt.Println("day2, task 1: ", day2Part1, ", took", (time.Now().UnixMilli() - startday2Part1), "ms")
+    startday2Part2 := time.Now().UnixMilli()
+    day2Part2 := day2.Part2("day2/day2.txt")
+    fmt.Println("day2, task 2: ", day2Part2, ", took", (time.Now().UnixMilli() - startday2Part2), "ms")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/gossie/adventofcode2024
 
-go 1.19
+go 1.22
